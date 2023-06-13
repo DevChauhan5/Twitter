18ca897601f73c69dddac071b2b4d43ff0ebdedd
DevChauhan5 <devchauhanthedev@gmail.com>
2023-06-13 21:29:18 +0530

    Tweet Component

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,21 +1,7 @@
 import { Text, View, Image, StyleSheet } from "react-native";
-import tweets from "../assets/data/tweets";
 import { TweetType } from "../types";
-import { Entypo, EvilIcons } from "@expo/vector-icons";
-
-type IconButtonProps = {
-  icon: React.ComponentProps<typeof EvilIcons>["name"];
-  text?: string | number;
-};
-
-const IconButton = ({ icon, text }: IconButtonProps) => {
-  return (
-    <View style={{ flexDirection: "row", alignItems: "center" }}>
-      <EvilIcons name={icon} size={22} color="gray" />
-      <Text style={{ fontSize: 12, color: "gray" }}>{text}</Text>
-    </View>
-  );
-};
+import { Entypo } from "@expo/vector-icons";
+import IconButton from "./IconButton";
 
 type TweetProps = {
   tweet: TweetType;
