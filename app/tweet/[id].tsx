1a1a7f43bfacbb3924642197cb658380f40fbd13
DevChauhan5 <devchauhanthedev@gmail.com>
2023-06-14 19:33:06 +0530

    Tweet Page

---
+++ app/tweet/[id].tsx
@@ -0,0 +1,13 @@
+import { useSearchParams } from "expo-router";
+import tweets from "../../assets/data/tweets";
+import Tweet from "../../components/Tweet";
+import { Text } from "react-native";
+
+export default function TweetScreen() {
+  const { id } = useSearchParams();
+  const tweet = tweets.find((t) => t.id === id);
+  if (!tweet) {
+    return <Text>Tweet {id} not found!</Text>;
+  }
+  return <Tweet tweet={tweet} />;
+}
